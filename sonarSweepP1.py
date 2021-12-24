c3062265550861467c32bacbf00336dbc8767aa8
ChanceMaker <cedrickbaker7@gmail.com>
2021-12-24 00:25:44 -0600

    init push for sonarSweepsP1

--- sonarSweepP1.py
+++ sonarSweepP1.py
@@ -1,5 +1,15 @@
 def main():
-    print("init")
+    a_file = open("inputs/sonarSweepP1Input.txt")
+    f = open("outputs/demofile2.txt", "a")
+    lines = a_file.readlines()
+    for val in lines:
+        
+        if(lines.index(val) == 0):
+            f.write("")
+            print("{} (N/A - no previous measurement)".format(val))
+        else:
+            print("{} We in ".format(val))
 
+    f.close()
 if __name__ == "__main__":
     main()
